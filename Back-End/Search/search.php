55eff36ed88ee75a9a7f7f95d1bf4c1dab8de186
mendesleu <leonardo.mendes10@hotmail.com>
2021-12-19 15:42:49 -0300

    Código de pesquisa para Search em BD

---
+++ Back-End/Search/search.php
@@ -0,0 +1,20 @@
+<?php
+
+    $search = isset($_GET['search']) ? $_GET['search'] : "";
+    $search = trim($search);
+
+    $select = "SELECT * FROM posts WHERE tags LIKE '%$search%' OR title LIKE '%$search%' OR category LIKE '%$search%'";
+    // Like por palavras no meio dos dados
+    $query = mysqli_query($conn, $select);
+
+    if ($query -> num_rows > 0) {
+        while ($row = $query -> fetch_assoc()) {
+
+?>
+
+<!-- Código HTML -->
+
+<?php
+        }
+    }
+?>
